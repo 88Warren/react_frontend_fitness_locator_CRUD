560d6b1301645bf53f815cf92bb64cdf273d9d68
laurawarren88 <laura@enterpriseautomation.co.uk>
2025-01-15 09:58:49 +0000

    Model Added

---
+++ models/activitiesModel.jsx
@@ -0,0 +1,21 @@
+/**
+* @typedef {Object} Place
+* @property {number} id
+* @property {string} name 
+* @property {string} vicinity
+* @property {string} city 
+* @property {string} postcode 
+* @property {string} phone
+* @property {string} email
+* @property {string} website
+* @property {string} openingHours
+* @property {string} type 
+* @property {string} description
+// * @property {string} photo
+// * @property {string} facilities_image
+// * @property {number} latitude 
+// * @property {number} longitude 
+* @property {string} createdAt
+* @property {string} updatedAt
+* @property {string} deletedAt
+*/
